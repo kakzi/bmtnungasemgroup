770e81504f71173176ff95981738942e5c0b6bb5
Moh Fatkur Rozi <kakzi.sholarship@gmail.com>
2025-01-29 20:52:32 +0700

    add whatsapp sending

--- app/Http/Controllers/Api/AttendanceController.php
+++ app/Http/Controllers/Api/AttendanceController.php
@@ -93,6 +93,27 @@ class AttendanceController extends Controller
                             'address' => $request->address
                         ]
                     );
+
+                    $token = "AGf3RDCCMB5RipQgjaY14AsTCyAFtrAUzLnGRCGqiMQfKyiePo";
+                    $phone= "085155105056"; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
+                    $message = "Testing by API ruangwa";
+
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$phone.'&message='.$message,
+                    ));
+                    $response = curl_exec($curl);
+                    curl_close($curl);
+                    echo $response;
+
                     return response()->json(
                         [
                             'message' => 'Absensi Check in berhasil di kirim!',
@@ -115,6 +136,27 @@ class AttendanceController extends Controller
                             'address' => $request->address
                         ]
                     );
+
+
+                    $token = "AGf3RDCCMB5RipQgjaY14AsTCyAFtrAUzLnGRCGqiMQfKyiePo";
+                    $phone= "085155105056"; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
+                    $message = "Testing by API ruangwa";
+
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$phone.'&message='.$message,
+                    ));
+                    $response = curl_exec($curl);
+                    curl_close($curl);
+                    echo $response;
                     return response()->json(
                         [
                             'message' => 'Absensi Check in berhasil di kirim!',
@@ -179,7 +221,25 @@ class AttendanceController extends Controller
                             'address' => $request->address
                         ]
                     );
+                    $token = "AGf3RDCCMB5RipQgjaY14AsTCyAFtrAUzLnGRCGqiMQfKyiePo";
+                    $phone= "085155105056"; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
+                    $message = "Testing by API ruangwa";
 
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$phone.'&message='.$message,
+                    ));
+                    $response = curl_exec($curl);
+                    curl_close($curl);
+                    echo $response;
                     return response()->json(
                         [
                             'message' => 'Absensi Check out berhasil di kirim!',
@@ -202,7 +262,25 @@ class AttendanceController extends Controller
                             'address' => $request->address
                         ]
                     );
+                    $token = "AGf3RDCCMB5RipQgjaY14AsTCyAFtrAUzLnGRCGqiMQfKyiePo";
+                    $phone= "085155105056"; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
+                    $message = "Testing by API ruangwa";
 
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$phone.'&message='.$message,
+                    ));
+                    $response = curl_exec($curl);
+                    curl_close($curl);
+                    echo $response;
                     return response()->json(
                         [
                             'message' => 'Absensi Check out berhasil di kirim!',
@@ -225,7 +303,25 @@ class AttendanceController extends Controller
                             'address' => $request->address
                         ]
                     );
+                    $token = "AGf3RDCCMB5RipQgjaY14AsTCyAFtrAUzLnGRCGqiMQfKyiePo";
+                    $phone= "085155105056"; //untuk group pakai groupid contoh: 62812xxxxxx-xxxxx
+                    $message = "Testing by API ruangwa";
 
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://app.ruangwa.id/api/send_message',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => 'token='.$token.'&number='.$phone.'&message='.$message,
+                    ));
+                    $response = curl_exec($curl);
+                    curl_close($curl);
+                    echo $response;
                     return response()->json(
                         [
                             'message' => 'Absensi Check out berhasil di kirim!',
